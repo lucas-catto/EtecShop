8df05aa0ce1d02122ede1dccca64aa85596d2542
lucas-catto <lucascattosilva2018@gmail.com>
2024-06-03 08:10:25 -0300

    Projeto Finalizado.

---
+++ EtecShop/ViewModels/HomeVM.cs
@@ -0,0 +1,8 @@
+using EtecShop.Models;
+namespace EtecShop.ViewModels;
+
+public class HomeVM
+{
+    public List<Categoria> Categorias { get; set; }
+    public List<Produto> Produtos { get; set; }
+}
